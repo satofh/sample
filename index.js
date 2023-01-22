6c1167a20ada45584331c63e7a9118b5a9c2955e
satofh <78581421+satofh@users.noreply.github.com>
2023-01-22 19:15:55 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var ua = window.navigator.userAgent.toLowerCase();
+let str = document.getElementById("str");
+
+if (ua.indexOf("iphone") !== -1) {
+    str.style.fontSize = '20px';
+} else if (ua.indexOf("mac os x") !== -1) {
+    str.style.fontSize = '100px';
+} else if (ua.indexOf("ipad") !== -1) {
+    str.style.fontSize = '40px';
+}
