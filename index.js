3d4632a0a6fb649a42a92dd25220b19961cdea08
satofh <78581421+satofh@users.noreply.github.com>
2023-01-22 19:26:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var ua = window.navigator.userAgent.toLowerCase();
 let str = document.getElementById("str");
 
-if (ua.indexOf("iphone") !== -1) {
-    str.style.fontSize = '40px';
-} else if (ua.indexOf("mac os x") !== -1) {
+if (ua.indexOf("mac os x") !== -1) {
     str.style.fontSize = '100px';
 } else if (ua.indexOf("ipad") !== -1) {
+    str.style.fontSize = '40px';
+} else if (ua.indexOf("iphone") !== -1) {
+    str.style.fontSize = '40px';
+} else {
     str.style.fontSize = '40px';
 }
