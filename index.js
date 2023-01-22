a6049d79c2ed165a568014b7ef17d8cab9e6a082
satofh <78581421+satofh@users.noreply.github.com>
2023-01-22 19:22:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var ua = window.navigator.userAgent.toLowerCase();
 let str = document.getElementById("str");
 
 if (ua.indexOf("iphone") !== -1) {
-    str.style.fontSize = '20px';
+    str.style.fontSize = '40px';
 } else if (ua.indexOf("mac os x") !== -1) {
     str.style.fontSize = '100px';
 } else if (ua.indexOf("ipad") !== -1) {
